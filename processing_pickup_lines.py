0c50f13ac35e0c796dc4e630d99be5717c366c85
elizabethtawes <elizabeth.tawes@students.olin.edu>
2018-04-19 21:57:48 -0400

    making mini databases

--- processing_pickup_lines.py
+++ processing_pickup_lines.py
@@ -1,5 +1,10 @@
 import pickle
 
 dict_of_pickup_lines = pickle.load(open('pickuplines.pickle', 'rb'))
-
-print(dict_of_pickup_lines)
+dict_of_olin = pickle.load(open('olinlines.pickle', 'rb'))
+dict_of_star_wars = pickle.load(open('star_wars.pickle', 'rb'))
+dict_of_music = pickle.load(open('music.pickle', 'rb'))
+dict_of_HIMYM = pickle.load(open('HIMYM.pickle', 'rb'))
+dict_of_GOT = pickle.load(open('GOT.pickle', 'rb'))
+dict_of_disney = pickle.load(open('disney.pickle', 'rb'))
+dict_of_biochem = pickle.load(open('biochem.pickle', 'rb'))
