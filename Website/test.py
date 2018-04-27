2f2834903689ae5279cf83a8d59c527623c757a8
epan547 <epan547@gmail.com>
2018-04-27 15:12:42 -0400

    emma

--- Website/test.py
+++ Website/test.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('index2.html')
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/feedback')
+def feedback():
+    return hello.html
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
